3f844246511ad213ca95288b2672ac17d10247c0
codepnw <idchokms01@outlook.com>
2024-04-30 21:01:15 +0700

    database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/codepnw/ecom-api/cmd/api"
+	"github.com/codepnw/ecom-api/config"
+	"github.com/codepnw/ecom-api/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQL(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initDB(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initDB(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: connected!")
 }
